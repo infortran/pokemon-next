1c61367ab5fbf902eef19b4660e55008f1857138
Freddy Perez <freddy.perez.trabajos@gmail.com>
2022-08-19 00:24:38 -0400

    getStatic Pokemons, list with id and name

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,39 @@
-import { Button } from '@nextui-org/react'
-import type { NextPage } from 'next'
-import {MainLayout} from '../components/layouts'
 
-const Home: NextPage = () => {
-  return (
-    <>
-    <MainLayout title="Mi primera APP">
-      <Button>Hola Mundo</Button>
-      </MainLayout>
-    </>
-  )
+import type { NextPage, GetStaticProps } from 'next'
+import { pokeApi } from '../api'
+import { MainLayout } from '../components/layouts'
+import { PokemonListResponse, SmallPokemon } from '../interfaces'
+
+interface Props {
+	pokemons: SmallPokemon[]
+}
+
+const Home: NextPage<Props> = (props) => {
+	console.log(props)
+	return (
+		<>
+			<MainLayout title="Listado de pokemons">
+				<ul>
+					{
+						props.pokemons.map((pokemon, i) => <li key={i}>{i + 1} - {pokemon.name}</li>)
+					}
+					
+				</ul>
+			</MainLayout>
+		</>
+	)
+}
+
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+	
+	const { data } = await pokeApi.get<PokemonListResponse>('pokemon?limit=151')
+	
+	return {
+		props: {
+			pokemons: data.results
+		}, // will be passed to the page component as props
+	}
 }
 
 export default Home
