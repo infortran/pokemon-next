b064eb6b370c37c1de1bab89e3ab5b68ab4c38b0
Freddy Perez <freddy.perez.trabajos@gmail.com>
2022-08-22 00:37:11 -0400

    add: pokemon pdp page, favorite page structure

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,33 +1,59 @@
-import { Image, Spacer, Text, useTheme } from "@nextui-org/react"
+import { Container, Image, Link, Spacer, Text, useTheme } from "@nextui-org/react"
+import { useRouter } from "next/router"
+import NextLink from 'next/link'
 
 
 
 export const Navbar = () => {
     const { theme } = useTheme()
+    // const router = useRouter()
+    // const redirectHome = () => {
+    //     router.push('/')
+    // }
 
     return (
         <div
             style={{
-                display:'flex',
-                width:'100%',
-                flexDirection:'row',
-                alignItems:'center',
-                justifyContent:'start',
-                padding:'0 20px',
-                backgroundColor:theme?.colors.gray100.value,
+                display: 'flex',
+                width: '100%',
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'start',
+                padding: '0 20px',
+                backgroundColor: theme?.colors.gray100.value,
 
             }}
+
+
         >
-            <Image 
+
+            <Image
                 src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png"
                 alt="icono de la app"
                 width={70}
                 height={70}
             />
-            <Text color="white" h2>P</Text>
-            <Text color="white" h3>okemon</Text>
+            {/* <div style={{ display:'flex', alignItems: 'center',cursor:'pointer'}}
+                onClick={ redirectHome }
+            >
 
-            <Spacer css={{flex:1}} />
+                <Image
+                    src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/150.png"
+                    alt="icono de la app"
+                    width={70}
+                    height={70}
+                />
+                
+            </div> */}
+
+            <NextLink href="/" passHref>
+                <Link>
+                    <Text color="white" h2>P</Text>
+                    <Text color="white" h3>okemon</Text>
+                </Link>
+            </NextLink>
+
+            <Spacer css={{ flex: 1 }} />
 
             <Text color="white">Favoritos</Text>
         </div>
--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,4 +1,6 @@
+import { Button, Card, Container, Grid, Text } from "@nextui-org/react"
 import { GetStaticPaths, GetStaticProps } from "next"
+import Image from "next/image"
 import { FC } from "react"
 import { pokeApi } from "../../api"
 import { MainLayout } from "../../components/layouts"
@@ -13,7 +15,62 @@ const PokemonPage: FC<Props> = ({pokemon}) => {
     console.log(pokemon)
     return (
         <MainLayout>
-            <h1>{pokemon.name}</h1>
+            <Grid.Container css={{ marginTop: '5px'}} gap={ 2 }>
+                <Grid xs={ 12 } sm={ 4 }>
+                    <Card isHoverable css={{ padding: '30px' }}>
+                        <Card.Body>
+                            <Card.Image 
+                                src={ pokemon.sprites.other?.dream_world.front_default || '/no-image.png' }
+                                alt={ pokemon.name }
+                                width="100%"
+                                height={ 200 }
+                            />
+                        </Card.Body>
+                    </Card>
+                </Grid>
+                <Grid xs={ 12 } sm={ 8 }>
+                    <Card>
+                        <Card.Header css={{ display: 'flex', justifyContent: 'space-between'}}>
+                            <Text h1 transform="capitalize">{ pokemon.name }</Text>
+                            <Button
+                                color="gradient"
+                                ghost
+                            >
+                                Guardar en favoritos
+                            </Button>
+                        </Card.Header>
+                        <Card.Body>
+                            <Text size={ 30 }>Sprites:</Text>
+                            <Container direction="row" display="flex" gap={ 0 }>
+                                <Image 
+                                    src={ pokemon.sprites.front_default }
+                                    alt={ pokemon.name }
+                                    width={ 100 }
+                                    height={ 100 }
+                                />
+                                <Image 
+                                    src={ pokemon.sprites.back_default }
+                                    alt={ pokemon.name }
+                                    width={ 100 }
+                                    height={ 100 }
+                                />
+                                <Image 
+                                    src={ pokemon.sprites.front_shiny }
+                                    alt={ pokemon.name }
+                                    width={ 100 }
+                                    height={ 100 }
+                                />
+                                <Image 
+                                    src={ pokemon.sprites.back_shiny }
+                                    alt={ pokemon.name }
+                                    width={ 100 }
+                                    height={ 100 }
+                                />
+                            </Container>
+                        </Card.Body>
+                    </Card>
+                </Grid>
+            </Grid.Container>
         </MainLayout>
     )
 }
